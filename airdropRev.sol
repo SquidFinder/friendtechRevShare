dfd12c581d44124767b59dbeae7e18bef43fcbfa
zeroExSquid <squid.finder.one@gmail.com>
2023-08-22 19:58:19 -0400

    update

--- airdropRev.sol
+++ airdropRev.sol
@@ -11,38 +11,56 @@ interface FriendtechSharesV1 {
 
 }
 
+/*
+    Balance must be added to contract prior to executing the addToWhitelist function 
+    which is a snapshot function based on freindtech.share holder balance
+    calling the sharesBalance() from the friendtech.share contract.
+*/
+
 contract OnChainWhitelistContract is Ownable {
 
     //FriendtechSharesV1 immutable friendTech = FriendtechSharesV1(0xCF205808Ed36593aa40a44F10c7f7C2F67d4A4d4);
     FriendtechSharesV1 immutable friendTech = FriendtechSharesV1(0x473327ee9C9c7cF4144db5FbBEaAA173C7EDdb6a);
     address public friendTechSubject; 
 
-    uint256 public supply;
-    uint256 internal round;
-    uint256 public uniqueHolders;
-    address internal token;
+    uint256 public round;
+    address public token;
 
-    mapping(address => bool) public whitelist;
-    //Balance reflects the holders balance at the time of the last snapshot.
-    mapping(address => uint256) public holderBalance;
-    mapping(uint256 => uint256) internal roundBalance;
-    mapping(uint256 => uint256) internal dropCount;
+        //All mappings are called by [round] as the first parameter.   [round][address] => whitelist
+    mapping(uint256 => mapping(address => bool)) public whitelist;
+        //holders balance at the time of the last snapshot. [round][address] => holderBalance 
+    mapping(uint256 => mapping(address => uint256)) public holderBalance;
+        //decided amount per share at the time of snapshot [round] => roundBalance 
+    mapping(uint256 => uint256) public roundBalance;
+        //calculates airdrop amount for round depending on holderBalance [round][address] => airdropValue;
+    mapping(uint256 => mapping(address => uint256)) public airdropValue;
+        //holders count of shares at the time of snapshot [round] => holderCount
+    mapping(uint256 => uint256) public holderCount;
+        //tracks the number of completed airdrops [round] => dropCount
+    mapping(uint256 => uint256) public dropCount;
+        //tracks if airdrop has been completed in a round to a specific address
+    mapping(uint256 => mapping(address => bool)) public airdropToUserStatus;
 
         //0x875D6ec5C6293B12BD391CCE7faaF9F5df37afFC
     constructor(address _friendTechSubject){
         friendTechSubject = _friendTechSubject;
+        token = 0x5104d35A6dE00b19cd5BD0649e3c31c7469fbF1A;
     }
 
     function updateRewardsToken(address _token) public onlyOwner {
         token = _token;
     }
 
-    function getSharesSupply() internal returns (uint256) {
-        supply = friendTech.sharesSupply(friendTechSubject);
-        return supply;
+    function rewardsTokenBalance() public view returns(uint256) {
+        return IERC20(token).balanceOf(address(this));
     }
 
-    function getShareCount(address user) internal view returns (uint256) {
+    function getSharesSupply() public view returns (uint256) {
+        return friendTech.sharesSupply(friendTechSubject);
+    }
+    //0x5B34F85e7d8DBE918c90081d2b799B4480B87E3B, 0x25dbF89A11a44BDa6E500a22e5E5D52d3b41eD42
+
+    function getShareCount(address user) public view returns (uint256) {
         return friendTech.sharesBalance(friendTechSubject, user);
     }
 
@@ -52,23 +70,26 @@ contract OnChainWhitelistContract is Ownable {
     function addToWhitelist(address[] calldata toAddAddresses) 
     external onlyOwner
     {
+        //require(IERC20(token).balanceOf(address(this)) > 0);
+        
         if(roundBalance[round] == 0) {
             calculateAirdropAmount();
         }
 
         for (uint i = 0; i < toAddAddresses.length; i++) {
             uint256 userBalance = getShareCount(toAddAddresses[i]);
-            if(userBalance > 0) {
-                whitelist[toAddAddresses[i]] = true;
-                holderBalance[toAddAddresses[i]] = userBalance;
-                uniqueHolders += 1;
+            if(userBalance > 0 && whitelist[round][toAddAddresses[i]] == false) {
+                whitelist[round][toAddAddresses[i]] = true;
+                holderCount[round] += 1;
             } 
 
-            if(userBalance == 0 && whitelist[toAddAddresses[i]] == true) {
-                delete whitelist[toAddAddresses[i]];
-                holderBalance[toAddAddresses[i]] = userBalance;
-                uniqueHolders -= 1;
+            if(userBalance == 0 && whitelist[round][toAddAddresses[i]] == true) {
+                delete whitelist[round][toAddAddresses[i]];
+                holderCount[round] -= 1;
             }
+
+            holderBalance[round][toAddAddresses[i]] = userBalance;
+            airdropValue[round][toAddAddresses[i]] = roundBalance[round] * userBalance;
         }
     }
 
@@ -80,9 +101,9 @@ contract OnChainWhitelistContract is Ownable {
     {
         for (uint i = 0; i < toRemoveAddresses.length; i++) {
             uint256 userBalance = getShareCount(toRemoveAddresses[i]);
-            delete whitelist[toRemoveAddresses[i]];
-            holderBalance[toRemoveAddresses[i]] = userBalance;
-            uniqueHolders -= 1;
+            delete whitelist[round][toRemoveAddresses[i]];
+            holderBalance[round][toRemoveAddresses[i]] = userBalance;
+            holderCount[round] -= 1;
         }
     }
 
@@ -94,11 +115,14 @@ contract OnChainWhitelistContract is Ownable {
     function airdropTowhitelist(address[] calldata toAddresses) external
     {
         for (uint i = 0; i < toAddresses.length; i++) {
-            if(whitelist[toAddresses[i]]) {
-                IERC20(token).transfer(toAddresses[i], roundBalance[round]);
-                dropCount[round] += 1;
+            if(whitelist[round][toAddresses[i]]) {
+                if(!airdropToUserStatus[round][toAddresses[i]]){
+                    airdropToUserStatus[round][toAddresses[i]] = true;
+                    dropCount[round] += 1;
+                    IERC20(token).transfer(toAddresses[i], roundBalance[round]);
+                }
             }
-            if(uniqueHolders <= dropCount[round]) {
+            if(holderCount[round] <= dropCount[round]) {
                 round += 1;
             } 
         }
